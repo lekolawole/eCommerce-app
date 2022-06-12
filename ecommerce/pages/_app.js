8da667de4ad77b1fff659f3ddfa19eb05fbf10a8
lekolawole <lekolawole@gmail.com>
2022-06-12 10:43:20 -0400

    Added Cart to navbar & Global State

--- ecommerce/pages/_app.js
+++ ecommerce/pages/_app.js
@@ -2,12 +2,17 @@ import React from 'react';
 import { Layout } from '../components';
 
 import '../styles/globals.css'
+import { StateContext } from '../context/StateContext';//allows you to receive data anywhere in the app
+import { Toaster } from 'react-hot-toast';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
     )
 }
 
--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -3,10 +3,12 @@ import index from '..';
 import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineStar, AiFillStar } from 'react-icons/ai';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';// uses state as a hook
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
-  const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState(0);
+  const { decQty, incQty, qty, onAdd } = useStateContext();
 
   return (
     <div>
@@ -43,13 +45,13 @@ const ProductDetails = ({ product, products }) => {
           <div className='quantity'>
             <h3>Quantity: </h3>
             <p className='quantity-desc'>
-              <span className='minus' onClick=''><AiOutlineMinus /></span>
-              <span className='num' onClick=''>0</span>
-              <span className='plus' onClick=''><AiOutlinePlus /></span>
+              <span className='minus' onClick={decQty}><AiOutlineMinus /></span>
+              <span className='num' onClick=''>{qty}</span>
+              <span className='plus' onClick={incQty}><AiOutlinePlus /></span>
             </p>
           </div>
           <div className='buttons'>
-            <button type='button' className='add-to-cart' onClick=''>Add to Cart</button>
+            <button type='button' className='add-to-cart' onClick={() => onAdd(product, qty)}>Add to Cart</button>
             <button type='button' className='buy-now' onClick=''>Buy Now</button>
           </div>
         </div>
