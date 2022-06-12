f50d24dcb941b31e9d4e0ddb45d033d98cbb9774
lekolawole <lekolawole@gmail.com>
2022-06-12 15:15:49 -0400

    Update [slug].js

--- ecommerce/pages/product/[slug].js
+++ ecommerce/pages/product/[slug].js
@@ -25,7 +25,7 @@ const ProductDetails = ({ product, products }) => {
           <div className='small-images-container'>
             {image?.map((item, i) => (
               <img
-                //key={index}
+                key={i}
                 src={urlFor(item)}
                 className={i === index ? 'small-image selected-image' : 'small-image'}
                 onMouseEnter={() => setIndex(i)}
